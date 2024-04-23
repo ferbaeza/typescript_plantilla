0bbbfab653d53a7417c8c404c8ac0ad696d6f3b5
Fernando Baeza <fernando.baeza@zataca.com>
2024-04-23 08:33:22 +0000

    husky

--- husky.config.js
+++ husky.config.js
@@ -3,7 +3,7 @@ const tasks = arr => arr.join(' && ');
 module.exports = {
   hooks: {
     'pre-commit': tasks([
-      'node ./.husky/qa/precommit.js',
+      'node ./husky/qa/precommit.js',
     ]),
     'post-merge': tasks([
       'echo "Actualizacion subida !" . date().toString()',
